a28fafebd2edcccb24f76f881c9635a7062f1f80
sanu <sanuptpm@gmail.com>
2017-01-12 18:00:22 +0530

    added more function for delete, move, upload

--- imap_response.py
+++ imap_response.py
@@ -1,5 +1,8 @@
 import imaplib
 import re
+import email
+import time
+import email.message
 
 from imap_conf import open_connection
 
@@ -58,6 +61,128 @@ def searching_messages():
         typ, msg_ids = c.search(None, 'ALL')
         print mailbox_name, typ, msg_ids  
 
+# check for search criteria
+def search_criteria():
+    typ, mailbox_data = c.list()
+    for line in mailbox_data:
+        flags, delimiter, mailbox_name = parse_list_response(line)
+        c.select(mailbox_name, readonly=True)
+        # Search for mail with subject m
+        typ, msg_ids = c.search(None, '(SUBJECT "m")')
+        # Search for mail with from contain "s" and subject contain "m"
+        # typ, msg_ids = c.search(None, '(FROM "s" SUBJECT "m")')
+        print mailbox_name, typ, msg_ids 
+
+# feaching content from inbox and display it. 
+def fetching_messages():
+    
+    c.select('INBOX', readonly=True)
+    
+    print 'HEADER:'
+    typ, msg_data = c.fetch('1', '(BODY.PEEK[HEADER])')
+    for response_part in msg_data:
+        if isinstance(response_part, tuple):
+            print response_part[1]
+    
+    print 'BODY TEXT:'
+    typ, msg_data = c.fetch('1', '(BODY.PEEK[TEXT])')
+    for response_part in msg_data:
+        if isinstance(response_part, tuple):
+            print response_part[1]
+
+    print '\nFLAGS:'
+    typ, msg_data = c.fetch('1', '(FLAGS)')
+    for response_part in msg_data:
+        print response_part
+        print imaplib.ParseFlags(response_part)
+
+# Retrieve the entire message as an RFC 2822 formatted mail message
+def whole_messages():
+    c.select('INBOX', readonly=True)
+    
+    typ, msg_data = c.fetch('1', '(RFC822)')
+    for response_part in msg_data:
+        if isinstance(response_part, tuple):
+            msg = email.message_from_string(response_part[1])
+            for header in [ 'subject', 'to', 'from' ]:
+                print '%-8s: %s' % (header.upper(), msg[header])
+
+# To add a new message to a mailbox
+def uploading_messages():
+    new_message = email.message.Message()
+    new_message.set_unixfrom('pymotw')
+    new_message['Subject'] = 'subject goes here'
+    new_message['From'] = 'sanu@example.com'
+    new_message['To'] = 'sanu@example.com'
+    new_message.set_payload('This is the body of the message.\n')
+
+    print new_message
+
+    c.append('INBOX', '', imaplib.Time2Internaldate(time.time()), str(new_message))
+    
+    c.select('INBOX')
+    typ, [msg_ids] = c.search(None, 'ALL')
+    for num in msg_ids.split():
+        typ, msg_data = c.fetch(num, '(BODY.PEEK[HEADER])')
+        for response_part in msg_data:
+            if isinstance(response_part, tuple):
+                print '\n%s:' % num
+                print response_part[1]
+
+# moving_and_copying_messages
+def moving_and_copying_messages():
+    # Find the "SEEN" messages in INBOX
+    c.select('INBOX')
+    typ, [response] = c.search(None, 'UNSEEN')
+    if typ != 'OK':
+        raise RuntimeError(response)
+    
+    # Create a new mailbox, "Today"
+    msg_ids = ','.join(response.split(' '))
+    typ, create_response = c.create('Today')
+    print 'CREATED Today:', create_response
+    
+    # Please uncomment "c.copy(msg_ids, 'Today')" for coping content from inbox to new mailbox "Today"
+    # Copy the messages
+    # print 'COPYING:', msg_ids
+    # c.copy(msg_ids, 'Today')
+    
+    # Look at the results
+    c.select('Today')
+    typ, [response] = c.search(None, 'ALL')
+    print 'COPIED:', response
+    
+# Delete message from mailbox
+def deleting_messages():
+    c.select('Today')
+
+    # What ids are in the mailbox?
+    typ, [msg_ids] = c.search(None, 'ALL')
+    print 'Starting messages:', msg_ids
+    
+    # # Find the message(s)
+    # typ, [msg_ids] = c.search(None, '(SUBJECT "subject")')
+    # msg_ids = ','.join(msg_ids.split(' '))
+    # print 'Matching messages:', msg_ids
+    
+    # # What are the current flags?
+    # typ, response = c.fetch(msg_ids, '(FLAGS)')
+    # print 'Flags before:', response
+    
+    # # Change the Deleted flag
+    # typ, response = c.store(msg_ids, '+FLAGS', r'(\Deleted)')
+    
+    # # What are the flags now?
+    # typ, response = c.fetch(msg_ids, '(FLAGS)')
+    # print 'Flags after:', response
+    
+    # # Really delete the message.
+    # typ, response = c.expunge()
+    # print 'Expunged:', response
+    
+    # What ids are left in the mailbox?
+    typ, [msg_ids] = c.search(None, 'ALL')
+    print 'Remaining messages:', msg_ids
 
 
 if __name__ == '__main__':
@@ -69,7 +194,14 @@ if __name__ == '__main__':
         # listing_mailboxes()
         # mailbox_status()
         # selecting_mailbox()
-        searching_messages()
+        # searching_messages()
+        # search_criteria()
+        # fetching_messages()
+        # whole_messages()
+        # uploading_messages()
+        # moving_and_copying_messages()
+        deleting_messages()
     
     finally:
         c.logout()
+        print('\nLogout success Goodbye!')
